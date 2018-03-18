7ad39bb5254d02baed81f072173da82b0beb49c3
jcheron <myaddressmail@gmail.com>
2018-03-18 14:32:19 +0100

    first commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,21 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('projects', function() {
+    this.route('new');
+    this.route('delete',{ path: 'delete/:project_id' });
+    this.route('update',{ path: 'update/:project_id' });
+  });
+  this.route('developers', function() {
+    this.route('new');
+    this.route('update',{ path: 'update/:developer_id' });
+    this.route('delete',{ path: 'delete/:developer_id' });
+  });
+  this.route('project',{ path: 'project/:project_id' });
+
+  this.route('story', function() {
+    this.route('new', {path: 'new/:project_id'});
+  });
 });
 
 export default Router;
---
+++ app/routes/projects/delete.js
@@ -0,0 +1,7 @@
+import DeleteRoute from '../../utils/delete-route';
+
+export default DeleteRoute.extend({
+  getRedirectRoute(){
+    return "projects";
+  }
+});
