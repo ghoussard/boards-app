5210d31487e8199bb5299a161d84c75b6aef2858
ghoussard <houssard.gregoire@gmail.com>
2018-03-20 20:02:41 +0100

    common form for create and update tag

--- app/routes/tags/new.js
+++ app/routes/tags/new.js
@@ -1,9 +1,15 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 import EmberObject from '@ember/object';
 
 export default Route.extend({
+  templateName: 'tags/form',
   model() {
-    return EmberObject.create();
+    return RSVP.hash({
+      newTag: EmberObject.create(),
+      isNew: true,
+      colors:['black','blue','green','orange','pink','purple','red','teal','yellow','positive','negative']
+    });
   },
   actions: {
     save:function(oldValue, newValue) {
